1b0ae3461a68eb01a5379d7d4ccf8ff08e5af26c
Konstantin <korchikov_k@mail.ru>
2025-07-28 11:16:41 +0300

    feat: add employees-list and employess-list-item components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,20 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 import './app.css';
 
 function App() {
   return (
     <div className="app">
-      <AppInfo/>
+      <AppInfo />
 
       <div className="search-panel">
-        <SearchPanel/>
+        <SearchPanel />
+        <AppFilter />
       </div>
+
+      <EmployeesList/>
     </div>
   );
 }
