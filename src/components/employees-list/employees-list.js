acab78388b8af0dea6b524de47a017e76f6e097f
Konstantin <korchikov_k@mail.ru>
2025-07-28 17:14:00 +0300

    feat: add key props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,11 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employess-list.css';
 
 const EmployeesList = ({ data }) => {
-  const elements = data.map(item => <EmployeesListItem {...item} />);
+  const elements = data.map(item => {
+    const {id, ...itemProps} = item;
+
+    return <EmployeesListItem key={id} {...itemProps} />;
+  });
 
   return (
     <ul className="app-list list-group">
